fa54491e5aa4ebdc140d36630e150d5e11bd899d
BALAJIMALI <balajij.bsc23@rvu.edu.in>
2023-10-19 16:16:46 +0530

    committed 10/19/2023, 4:16:44 PM

--- set01/problem12.c
+++ set01/problem12.c
@@ -10,3 +10,47 @@ void input_n_complex(int n, Complex c[n]);
 Complex add(Complex a, Complex b);
 Complex add_n_complex(int n, Complex c[n]);
 void output(int n, Complex c[n], Complex result);
+
+int main()
+{
+	int n;
+	n=get_n();
+	Complex C[n],sum;
+	input_n_complex(n,C);
+	sum=add_n_complex(n,C);
+	output(n,C,sum);
+	return 0;
+
+}
+int_get_n()
+{
+	int n;
+	printf("Enter size :");
+	scanf("%d",&n);
+	return n;
+
+}
+Complex input_complex()
+{
+ Complex r;
+ scanf("%f%f",&r.real,&r.imaginary);
+ return r;
+}
+void input_n_complex(int n,Complex C[n])
+{
+	int i;
+	for(i=0;i,n;i++)
+	{
+		C[i]=input_complex();
+
+	}
+}
+Complex add_complex(Complex a,Complex b)
+{
+	Complex sum;
+	sum.real=a.real+b.real;
+	sum.imaginary=a.imaginary+b.imaginary;
+	return sum;
+
+}
+Complex add_add_n
