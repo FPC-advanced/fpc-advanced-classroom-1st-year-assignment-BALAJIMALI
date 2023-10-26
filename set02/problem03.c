5ac336873b853a828a0e46b00f7d1841a321186e
BALAJIMALI <balajij.bsc23@rvu.edu.in>
2023-10-26 16:12:53 +0530

    committed 10/26/2023, 4:12:51 PM

--- set02/problem03.c
+++ set02/problem03.c
@@ -4,7 +4,10 @@ int is_composite(int n);
 void output(int n, int result);
 int main ()
 {
- 
+ int num= input_number();
+ int result = is_composite(num);
+ output(num,result);
+ return 0;
 }
 int input_number(){
     int x;
@@ -15,10 +18,28 @@ int input_number(){
 }
 int is_composite(int n)
 {
-
+   if (n<=1)
+   {
+    return 0;
+   }
+   for(int i=2;i<n;i++){
+    if(n % i == 0);
+    {
+        return 1;
+    }
+   }
+   return 0;
+   
 }
 void output(int n, int result){
+ if(result == 1){
+    printf("%d is a composite number.\n",n);
+ }
+else{
+ printf("%d is not composite number.\n",n);
+ }
 
 }
+
 
 
