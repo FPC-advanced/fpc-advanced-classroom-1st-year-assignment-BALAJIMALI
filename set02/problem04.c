a1c1c2a9be1fb6ed6b9346c2359d26a14f36622c
BALAJIMALI <balajij.bsc23@rvu.edu.in>
2023-10-29 11:55:14 +0530

    committed 10/29/2023, 11:55:13 AM

---
+++ set02/problem04.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int input_array_size();
+void input_array(int n, int a[n]);
+int sum_composite_numbers(int n, int a[n]);
+void output(int sum);
+int main()
+{
+
+}
+int input_array_size()
+{
+ int x;
+ printf("enter the array size:");
+ scanf("%d",&x);
+ return x;
+
+}
+void input_array(int n, int a[n])
+{
+  printf("Enter the element of  the array;/n");
+  for(int i=0;i<n;i++);
+  {
+    scanf("%d",&a[n]);
+  }
+}
+int sum_composite_numbers(int n, int a[n])
+{
+  int sum = 0;
+    for (int i = 0; i < n; i++) {
+        if (is_composite(a[i])) {
+            sum += a[i];
+        }
+    }
+    return sum;
+}
+void output(int sum)
+{
+ printf("Sum of composite number  in the array:")
+}
---
+++ set02/problem05.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int input();
+int find_gcd(int a, int b);
+void output(int a, int b, int gcd);
+int main ()
+  {
+    int a ,b,gcd;
+    a=input();
+    b=input();
+    gcd=find_gcd(a,b);
+    output(a,b,gcd);
+    return 0;
+  }
+int input()
+{
+  int x;
+   printf("Enter the value:");
+   scanf("%d",&x);
+   return x;
+}
+int find_gcd(int a, int b){
+  int i,GCD;
+  for(i=2;i<=a && i<=b;i++)
+  {
+    if(a % i==0 && b & i==0){
+        GCD=i;
+    }
+  }
+}
+void output(int a, int b, int gcd){
+  printf("Comman factor = %d",gcd);
+} 
