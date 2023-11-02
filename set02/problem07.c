09f58fcd0f63d286cdb9b01ed7c96f21b11c5eca
BALAJIMALI <balajij.bsc23@rvu.edu.in>
2023-11-02 16:26:16 +0530

    committed 11/2/2023, 4:26:14 PM

--- set02/problem07.c
+++ set02/problem07.c
@@ -9,5 +9,20 @@ void find_area(Traingle *t);
 void output(Triangle t);
 int main()
 {
-
+     Triangle t1 = input_triangle();
+    find_area(&t1);
+    output(t1);
+    return 0;
 }
+Triangle input_triangle(){
+    int t;
+    printf("Enter base and altitude /n");
+    scanf("%d",t);
+    }
+void find_area(Traingle *t){
+
+}   
+void output(Triangle t){
+    printf("The aera of trianglewith base %d and altitude %d is %d ,base,altitude,area ");
+} 
+
--- set03/problem01.c
+++ set03/problem01.c
@@ -4,13 +4,21 @@ float find_distance(float x1, float y1, float x2, float y2);
 void output(float x1, float y1, float x2, float y2, float distance);
 int main()
 {
-
+  float x1, y1, x2, y2, distance;
+    input(&x1, &y1, &x2, &y2);
+    distance = find_distance(x1, y1, x2, y2);
+    output(x1, y1, x2, y2, distance);
+    return 0;
 }
 void input(float *x1, float *y1, float *x2, float *y2){
+       printf("Enter the x and y coordinates of the first point: ");
+    scanf("%f %f", x1, y1);
+    printf("Enter the x and y coordinates of the second point: ");
+    scanf("%f %f", x2, y2);
 
 }
 float find_distance(float x1, float y1, float x2, float y2){
-
+    
 }
 void output(float x1, float y1, float x2, float y2, float distance){
     
