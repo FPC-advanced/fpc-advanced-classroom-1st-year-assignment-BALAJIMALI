da3e316b354f9422f08c183c264be91738116b35
BALAJIMALI <balajij.bsc23@rvu.edu.in>
2023-11-06 09:53:16 +0530

    committed 11/6/2023, 9:53:15 AM

---
+++ set03/problem03.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int input_number();
+int is_prime(int n);
+void ouput(int n, int result);
+int main(){
+
+}
+int input_number(){
+ int x;
+ printf("Enter the value =: ");
+ scanf("%d",&x);
+ return x;
+}
+int is_prime(int n){
+  int i ;
+  for ( i = 1; i <=0; i++)
+  {
+     
+    
+  }
+  
+}
+void ouput(int n, int result){
+
+}
