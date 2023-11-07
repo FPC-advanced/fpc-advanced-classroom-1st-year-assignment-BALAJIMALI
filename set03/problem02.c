c0751c0023f2a0e0b346ffb5158fcb82b183c688
BALAJIMALI <balajij.bsc23@rvu.edu.in>
2023-11-07 11:51:05 +0530

    committed 11/7/2023, 11:51:04 AM

--- set03/problem02.c
+++ set03/problem02.c
@@ -8,7 +8,7 @@ void output(float x1, float y1, float x2, float y2,float x3, float y3, int resul
 int main(){
     int result;
    float x1, y1, x2, y2, x3, y3;
-    input(&x1, &y1, &x2, &y2);
+    input(&x1, &y1, &x2, &y2,&x1 );
     result = find_distance(x1, y1, x2, y2);
     output(x1, y1, x2, y2, x3, y3 , result);
 }
