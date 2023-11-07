e50748897740e9c92890924887cbacb7a407fbb9
BALAJIMALI <balajij.bsc23@rvu.edu.in>
2023-11-07 11:47:05 +0530

    committed 11/7/2023, 11:47:04 AM

--- set03/problem02.c
+++ set03/problem02.c
@@ -12,7 +12,7 @@ int main(){
     output(x1, y1, x2, y2, x3, y3 , result);
 }
 void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3){
-
+  
 }
 int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3){
 
