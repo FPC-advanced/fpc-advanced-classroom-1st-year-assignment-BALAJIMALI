67460ccef13659c1075d2244b34165702653dcf4
BALAJIMALI <balajij.bsc23@rvu.edu.in>
2023-11-07 11:43:35 +0530

    committed 11/7/2023, 11:43:34 AM

--- set03/problem02.c
+++ set03/problem02.c
@@ -1,1 +1,7 @@
 #include<stdio.h>
+
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3);
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3);
+void output(float x1, float y1, float x2, float y2,float x3, float y3, int result);
+
+i
