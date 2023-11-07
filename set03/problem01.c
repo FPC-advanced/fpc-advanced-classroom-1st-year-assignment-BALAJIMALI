e20938733223d9beefa62565c72a617a0e254f47
BALAJIMALI <balajij.bsc23@rvu.edu.in>
2023-11-07 11:40:35 +0530

    committed 11/7/2023, 11:40:34 AM

--- set03/problem01.c
+++ set03/problem01.c
@@ -25,5 +25,5 @@ float find_distance(float x1, float y1, float x2, float y2){
     return distance;
 }
 void output(float x1, float y1, float x2, float y2, float distance){
-    printf("The distance between point (%f,%f) and (%f,%f) is %f",x1,x2,y1,y2);
+    printf("The distance between point (%f,%f) and (%f,%f) is %f",x1,x2,y1,y2,distance);
 }
