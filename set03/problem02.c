07eddf176315ef166c45f866504fbdefc0faf906
BALAJIMALI <balajij.bsc23@rvu.edu.in>
2023-11-07 11:52:05 +0530

    committed 11/7/2023, 11:52:04 AM

--- set03/problem02.c
+++ set03/problem02.c
@@ -8,8 +8,8 @@ void output(float x1, float y1, float x2, float y2,float x3, float y3, int resul
 int main(){
     int result;
    float x1, y1, x2, y2, x3, y3;
-    input(&x1, &y1, &x2, &y2,&x3 ,&y3);
-    result = find_distance(x1, y1, x2, y2);
+    input_triangle(&x1, &y1, &x2, &y2,&x3 ,&y3);
+    result = is_triangle(x1, y1, x2, y2,x3,y3);
     output(x1, y1, x2, y2, x3, y3 , result);
 }
 void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3){
