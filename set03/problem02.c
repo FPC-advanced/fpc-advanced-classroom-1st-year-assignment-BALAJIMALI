0337582976e8327158d2e1b8960e0ec5dc868187
BALAJIMALI <balajij.bsc23@rvu.edu.in>
2023-11-07 12:28:20 +0530

    committed 11/7/2023, 12:28:19 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -22,8 +22,25 @@ void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float
 }
 int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3){
   int result;
+  float d1= sqrt(((x2-x1)*2) + ((y2-y1)*2));
+  float d2= sqrt(((x3-x2)*2) + ((y3-y2)*2));
+  float d3= sqrt(((x1-x3)*2) + ((y1-y3)*2));
+
+  if(d1 + d2 > d3 && d2 + d3 > d1 && d1 +d3 > d3 ){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+  return result;
   
 }
 void output(float x1, float y1, float x2, float y2,float x3, float y3, int result){
-
+  if (result == 1)
+   { printf("The points (%f,%f), (%f,%f) and (%f,%f) do  from triangle \n",x1,x2,x3,y1,y2,y3,result);
+}
+else
+{
+   printf("The points (%f,%f), (%f,%f) and (%f,%f) do not from triangle \n",x1,x2,x3,y1,y2,y3,result);
+}
 }
