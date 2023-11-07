143052256a5cc8a1dcaec27679e56a6a2441330e
BALAJIMALI <balajij.bsc23@rvu.edu.in>
2023-11-07 11:47:35 +0530

    committed 11/7/2023, 11:47:34 AM

--- set03/problem02.c
+++ set03/problem02.c
@@ -12,7 +12,11 @@ int main(){
     output(x1, y1, x2, y2, x3, y3 , result);
 }
 void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3){
-  
+    printf("Enter the x and y coordinates of the first point: ");
+    scanf("%f %f", x1, y1);
+    printf("Enter the x and y coordinates of the second point: ");
+    scanf("%f %f", x2, y2);
+    printf("Enter the x and y ")
 }
 int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3){
 
