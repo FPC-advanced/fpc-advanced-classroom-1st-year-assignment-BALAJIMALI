1e4cf4b8d337cfd3b89abf3b2a28a19a9a41ffca
BALAJIMALI <balajij.bsc23@rvu.edu.in>
2023-11-09 21:10:16 +0530

    committed 11/9/2023, 9:10:15 PM

--- set03/problem06.c
+++ set03/problem06.c
@@ -12,3 +12,10 @@ int main(){
     index=sub_str_index(a,b);
     output(a,b,index);
 }
+void input_string(char* a, char* b){
+
+}
+int sub_str_index(char* string, char* substring){
+    
+}
+void output(char *string, char *substring, int index);
