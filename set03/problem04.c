6f695d19089f5ad7fc6e632d7cb0abfbb6bee49f
BALAJIMALI <balajij.bsc23@rvu.edu.in>
2023-11-09 21:02:16 +0530

    committed 11/9/2023, 9:02:14 PM

--- set03/problem04.c
+++ set03/problem04.c
@@ -16,10 +16,14 @@ int input(){
   scanf("%d",&x);
 }
 int find_fibo(int n){
-   if(n<=1)
-   return n;
-   return fibo(n-1) + fibo(n-2);
+     int a[n];
+    for(int i=2;i<=n;i++)
+    {
+        a[0]=0,a[1]=1;
+        a[i]=a[i-1]+a[i-2];
+    }
+    return a[n];
 }
 void output(int n, int fibo){
-  printf("fibo(%d)is=%d",n,fibo);
+  printf("fibo(%d)is = %d /n",n,fibo);
 }
