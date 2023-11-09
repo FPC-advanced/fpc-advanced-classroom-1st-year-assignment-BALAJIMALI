b7d28adb77ace2e0d4140bec53a146dff8f55f0c
BALAJIMALI <balajij.bsc23@rvu.edu.in>
2023-11-09 21:09:16 +0530

    committed 11/9/2023, 9:09:15 PM

--- set03/problem06.c
+++ set03/problem06.c
@@ -1,1 +1,14 @@
 #include<stdio.h>
+
+void input_string(char* a, char* b);
+int sub_str_index(char* string, char* substring);
+void output(char *string, char *substring, int index);
+
+int main(){
+    
+    char a[],b[100];
+    int index;
+    input_string(a,b);
+    index=sub_str_index(a,b);
+    output(a,b,index);
+}
