83c883230cf9d6b07a8c3375d45da0840f3bb219
BALAJIMALI <balajij.bsc23@rvu.edu.in>
2023-11-09 21:07:46 +0530

    committed 11/9/2023, 9:07:45 PM

--- set03/problem05.c
+++ set03/problem05.c
@@ -4,11 +4,21 @@ void init_array(int n, int a[n]);
 void erotosthenes_sieve(int n, int a[n]);
 void output(int n, int a[n]);
 int main(){
-
+ int n;
+    n=input_array_size();
+    int a[n];
+    int_array(n,a);
+    erotosthenes_sieve(n,a);
+    output(n,a);
+    return 0;
 }
 int input_array_size(){
-
+  int a;
+    printf("Enter the size of the array : \n");
+    scanf("%d",&a);
+    return a;
 }
+} 
 void init_array(int n, int a[n]){
 
 }
