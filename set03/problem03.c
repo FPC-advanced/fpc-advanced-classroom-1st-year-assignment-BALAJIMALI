99b62291e7ae741754bdd093296d8d184174a2ac
BALAJIMALI <balajij.bsc23@rvu.edu.in>
2023-11-09 21:06:16 +0530

    committed 11/9/2023, 9:06:15 PM

--- set03/problem03.c
+++ set03/problem03.c
@@ -19,18 +19,20 @@ int input_number(){
 }
 
 int is_prime(int n){
-  int i;
   
-  if (n <= 1) {
-      return 0;
-  }
-
-  for (i = 2; i <= n/2; i++) {
-    if (n % i == 0) {
-        return 0;
+    int result=0;
+    for(int i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            result++;
+        }
+        else
+        {
+            continue;
+        }
     }
-  }
-  return 1;
+    return result;
 }
 
 void output(int n, int result){
