e6ee6c9f92533e2bd86488aab5dea7a81bd01245
BALAJIMALI <balajij.bsc23@rvu.edu.in>
2023-11-09 16:03:38 +0530

    committed 11/9/2023, 4:03:37 PM

--- set03/problem03.c
+++ set03/problem03.c
@@ -2,33 +2,42 @@
 
 int input_number();
 int is_prime(int n);
-void ouput(int n, int result);
+void output(int n, int result);
+
 int main(){
   int n,result;
   n=input_number();
-  ouput(n,result);
+  result = is_prime(n);
+  output(n,result);
 }
+
 int input_number(){
  int x;
  printf("Enter the value =: ");
  scanf("%d",&x);
  return x;
 }
+
 int is_prime(int n){
   int i;
-
-
-  for ( i = 2; i <=n/2; i++)
-  {
-    if (n% i == 0)
-    {
-        result ++;
-        return result;
-    }
-    
-  }
   
+  if (n <= 1) {
+      return 0;
+  }
+
+  for (i = 2; i <= n/2; i++) {
+    if (n % i == 0) {
+        return 0;
+    }
+  }
+  return 1;
 }
-void ouput(int n, int result){
- printf("The number is prime %d",result);
+
+void output(int n, int result){
+    if(result == 1){
+        printf("The number %d is prime.\n", n);
+    }
+    else {
+        printf("The number %d is not prime.\n", n);
+    }
 }
