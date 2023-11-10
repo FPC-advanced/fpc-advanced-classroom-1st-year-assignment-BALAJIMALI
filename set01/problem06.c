9b4823597d61c7f9ba37dd7bea859dcdad2e2203
BALAJIMALI <balajij.bsc23@rvu.edu.in>
2023-11-10 13:30:32 +0530

    committed 11/10/2023, 1:30:31 PM

--- set01/problem06.c
+++ set01/problem06.c
@@ -26,14 +26,12 @@ void compare (int a,int b, int c,int *largest)
     if (a> b && a > c)
     {
         *largest=a;
-
     }
      else if ( b > c)
     {
         *largest = b;
     }
     else
-
     {
         *largest=c;
     }
