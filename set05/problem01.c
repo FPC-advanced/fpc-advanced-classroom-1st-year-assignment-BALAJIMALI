53e54b16224e68d0141fce43bd182666f1b80ef4
BALAJIMALI <balajij.bsc23@rvu.edu.in>
2023-11-20 09:37:07 +0530

    committed 11/20/2023, 9:37:06 AM

---
+++ set05/problem01.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <math.h>
+
+struct  _point
+{
+   float x;
+   float y;
+};
+
+typedef struct  _point point;
+ 
+ point input();
+ void dist(point a, point b,float *res);
+ void output( point a, point b, float res);
+
+ int main()
+ {
+    point a,b;
+    float res;
+    printf("Enter the first point coordinates\n");
+    a=input();
+    b=input();
+    dist(a,b,&res);
+    output(a,b,res);
+ }
+ point input()
+ {
+    point a;
+    printf("Enter the x-coordinate : \n");
+    scanf("%f",&a.x);
+    printf("Enter the y-coordinate : \n");
+    scanf("Enter the y coordinate : \n");
+    scanf("%f",&a.y);
+    return a;
+ }
+ void dist (point a, point b, float *res){
+    float dx=a.x -b.x;
+    float dy=a.y - b.y;
+    *res =sqrt (( dx *dx) +(dy * dy ));
+ }
+void output (point a, point b, float res )
+{
+    printf("THe Distance betwwen (%f,%f) and (%f ,%f) is %f",a.x,a.y,b.x,b.y,res);
+}
---
+++ set05/problem02.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void input_camel_details(float *radius, float *height, float *length);
+float find_weight(float radius, float height, float length);
+void output(float radius, float height, float length, float weight);
+
+int main ();
+{
+int radius ;
+
+}
+void input_camel_details(float *radius, float *height, float *length){
+
+}
+float find_weight(float radius, float height, float length){
+
+}
+void output(float radius, float height, float length, float weight){
+ printf(" The weight of the camel with radius:  ")
+}
