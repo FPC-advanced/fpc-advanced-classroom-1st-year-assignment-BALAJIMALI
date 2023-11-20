6cd7c4d52d2d704b63d28e06163f5b77eb61f984
BALAJIMALI <balajij.bsc23@rvu.edu.in>
2023-11-20 09:43:38 +0530

    committed 11/20/2023, 9:43:36 AM

--- set05/problem03.c
+++ set05/problem03.c
@@ -1,3 +1,14 @@
 #include <stdio.h>
 #include <math.h>
 
+struct camel {
+	float radius, height, length,weight;
+};
+
+typedef struct camel Camel;
+
+Camel input();
+float find_weight(Camel c);  //pass by value
+//or
+void find_weight(Camel *c); //passing address variable
+void output(Camel c);
