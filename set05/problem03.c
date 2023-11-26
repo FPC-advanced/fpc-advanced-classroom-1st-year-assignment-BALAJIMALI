d489d9594aa01359ad82998aaddbdab5c7a254a8
BALAJIMALI <balajij.bsc23@rvu.edu.in>
2023-11-26 19:31:10 +0530

    committed 11/26/2023, 7:31:10 PM

--- set05/problem03.c
+++ set05/problem03.c
@@ -8,33 +8,35 @@ struct camel {
 typedef struct camel Camel;
 
 Camel input();
-// float find_weight(Camel c);  //pass by value
-// //or
-void find_weight(Camel *c); //passing address variable
+void find_weight(Camel *c);  //pass by value
+//passing address variable
 void output(Camel c);
- 
- int main()
- {
+
+int main()
+{
     Camel c;
     c=input();
     find_weight(&c);
     output(c);
- }
- Camel input()
- {
+}
+Camel input()
+{
     Camel a;
-    printf("Enter  the radius of the camel: \n");
+    printf("Enter the radius of the camel: \n");
     scanf("%f",&a.radius);
     printf("Enter the height of the camel: \n");
     scanf("%f",&a.height);
     printf("Enter the length of the camel: \n");
-    scanf("%f",&a.length);
- }
- void find_weight(Camel *c){
+    scanf("%f",&a.length);   
+    return a;
+}
+void find_weight(Camel *c)
+{
     float pi=3.14;
     float rad=pow(c->radius,3);
-    c->weight =pi * rad * sqrt(c->height * c->length);
- }
- void output(Camel c){
-    printf("The weight of the camel with radius: %f,height :%f,lenght: %f is %f \n",c.radius,c.height,c.length,c.weight);
- }
+    c->weight = pi * rad * sqrt(c->height * c->length);
+}
+void output(Camel c)
+{
+    printf("The weight of the camel with radius: %f, height: %f, length: %f is %f \n",c.radius,c.height,c.length,c.weight);
+}
